fe263a418585f4a9165a288ef6c273dc7cb11990
yudi2802 <yudipablo999@gmail.com>
2025-01-07 15:41:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def home():
     return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8081)
 
 def keep_alive():
     t = Thread(target=run)
